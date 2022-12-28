3f72485ce366a4dbc8ba8d8262df3b90a7b920a9
Yann Lauwers <yann@embie.be>
2022-12-28 19:00:38 +0100

    feat: add first sectino basic

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,34 @@ const getDiagonal = (height: number, width: number) => {
 }
 
 export default function Home() {
-  const ref = useRef(null)
+  const ref = useRef<HTMLDivElement>(null)
 
   const [height, setHeight] = useState(0)
   const [width, setWidth] = useState(0)
+  const [text, setText] = useState("I'm Yann")
+
+  useEffect(() => {
+    let interval: any
+
+    const timer = setTimeout(() => {
+      setText(prevState => (prevState === "I'm Yann" ? "I'm a fullstack developer" : "I'm Yann"))
+
+      interval = setInterval(() => {
+        setText(prevState => (prevState === "I'm Yann" ? "I'm a fullstack developer" : "I'm Yann"))
+      }, 7000)
+    }, 5250)
+
+    return () => {
+      clearTimeout(timer)
+      clearInterval(interval)
+    }
+  }, [])
 
   useLayoutEffect(() => {
-    setHeight(ref.current.clientHeight)
-    setWidth(ref.current.clientWidth)
+    if (ref.current) {
+      setHeight(ref.current.clientHeight)
+      setWidth(ref.current.clientWidth)
+    }
   }, [])
 
   return (
@@ -27,7 +47,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="h-full z-0">
-        <div className="relative h-screen overflow-hidden" ref={ref}>
+        <div className="relative h-screen overflow-hidden hidden lg:block" ref={ref}>
           <div
             style={{
               height: `${getDiagonal(height, width)}px`,
@@ -36,12 +56,9 @@ export default function Home() {
             className="z-0 absolute top-1/2 left-1/2 bg-[conic-gradient(var(--tw-gradient-stops))] from-green-700 via-green-200/50 to-white duration-500 -translate-x-1/2 -translate-y-1/2 rotate"
           />
           <div className="relative z-10 flex h-full w-full">
-            <div className="w-1/2 h-full flex items-center">
-              <h1 className="mx-auto">
-                <span className="text-green-900 font-bold">Hey</span>
-                <br />
-                <span className="text-white">i&apos;m yann</span>
-              </h1>
+            <div className="h-full flex items-start justify-center mb-28 flex-col ml-28">
+              <h1 className="text-green-900 font-bold text-5xl xl:text-6xl">Hey</h1>
+              <h1 className="text-white text-5xl xl:text-6xl">{text}</h1>
             </div>
           </div>
         </div>

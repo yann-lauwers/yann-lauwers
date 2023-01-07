1b63b7fa2aebf9c9af31a1614c9c701fae756a7d
Yann Lauwers <yann@embie.be>
2023-01-07 13:52:49 +0100

    feat: add trivago import sorter and tailwindcss classes sorter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,30 @@
-import dynamic from "next/dynamic"
-import Head from "next/head"
-import { useEffect, useRef, useState } from "react"
-import { Section2Container as SecondSectionContainer } from "../components/homepage/section2"
-import useOnScreen from "../hooks/use-on-screen"
-const FirstSection = dynamic(() => import("../components/homepage/section1"))
-const SecondSection = dynamic(() => import("../components/homepage/section2"))
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
+
+import { Section2Container as SecondSectionContainer } from "../components/homepage/section2";
+import useOnScreen from "../hooks/use-on-screen";
+
+const FirstSection = dynamic(() => import("../components/homepage/section1"));
+const SecondSection = dynamic(() => import("../components/homepage/section2"));
 
 export default function Home() {
-  const [isClient, setIsClient] = useState(false)
-  const section2Ref = useRef<HTMLParagraphElement | null>(null)
-  const Section2RefValue = useOnScreen(section2Ref)
-  const [isSection2Visible, setIsSection2Visible] = useState(false)
+  const [isClient, setIsClient] = useState(false);
+  const section2Ref = useRef<HTMLParagraphElement | null>(null);
+  const Section2RefValue = useOnScreen(section2Ref);
+  const [isSection2Visible, setIsSection2Visible] = useState(false);
 
   useEffect(() => {
     if (!isSection2Visible) {
-      setIsSection2Visible(Section2RefValue)
+      setIsSection2Visible(Section2RefValue);
     }
-  }, [Section2RefValue, isSection2Visible])
+  }, [Section2RefValue, isSection2Visible]);
 
   useEffect(() => {
     // setTimeout(() =>
-    setIsClient(true)
+    setIsClient(true);
     // , 1000)
-  }, [])
+  }, []);
 
   return (
     <>
@@ -31,7 +33,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       {/* {!isClient ? null : ( */}
-      <main className="h-full z-0">
+      <main className="z-0 h-full">
         <FirstSection />
         <SecondSectionContainer ref={section2Ref}>
           {isSection2Visible && <SecondSection />}
@@ -39,5 +41,5 @@ export default function Home() {
       </main>
       {/* )} */}
     </>
-  )
+  );
 }

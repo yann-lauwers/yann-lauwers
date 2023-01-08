2ba1b52446c73ddaa6160daab51d798421e0f908
Yann Lauwers <yann@embie.be>
2023-01-08 12:16:22 +0100

    fix: should fix delay on uselayout while downloading page on web instead of local

--- src/components/homepage/section1.tsx
+++ src/components/homepage/section1.tsx
@@ -13,28 +13,37 @@ const Section1 = () => {
   const [width, setWidth] = useState(0);
   const [text, setText] = useState("I'm Yann");
 
+  const isRotating = height !== 0 && width !== 0;
+
+  // https://github.com/react-component/overflow/issues/6#issuecomment-819215239
+  const useIsomorphicLayoutEffect =
+    typeof window !== "undefined" ? useLayoutEffect : useEffect;
+
   useEffect(() => {
     let interval: any;
+    let timer: any;
 
-    const timer = setTimeout(() => {
-      setText((prevState) =>
-        prevState === "I'm Yann" ? "I'm a fullstack developer" : "I'm Yann"
-      );
-
-      interval = setInterval(() => {
+    if (isRotating) {
+      timer = setTimeout(() => {
         setText((prevState) =>
           prevState === "I'm Yann" ? "I'm a fullstack developer" : "I'm Yann"
         );
-      }, 7000);
-    }, 5250);
+
+        interval = setInterval(() => {
+          setText((prevState) =>
+            prevState === "I'm Yann" ? "I'm a fullstack developer" : "I'm Yann"
+          );
+        }, 5000);
+      }, 5000);
+    }
 
     return () => {
       clearTimeout(timer);
       clearInterval(interval);
     };
-  }, []);
+  }, [isRotating]);
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     if (ref.current) {
       setHeight(ref.current.clientHeight);
       setWidth(ref.current.clientWidth);
@@ -56,7 +65,7 @@ const Section1 = () => {
       <div className="relative z-10 flex h-full w-full">
         <div className="mb-28 ml-28 flex h-full flex-col items-start justify-center">
           <h1 className="text-5xl font-bold text-green-900 xl:text-6xl">Hey</h1>
-          <h1 className="text-5xl text-white xl:text-6xl">{text}</h1>
+          <h1 className="text-5xl text-red-500 xl:text-6xl">{text}</h1>
         </div>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,14 @@ import dynamic from "next/dynamic";
 import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
 
+import Section1 from "../components/homepage/section1";
 import { Section2Container as SecondSectionContainer } from "../components/homepage/section2";
 import useOnScreen from "../hooks/use-on-screen";
 
-const FirstSection = dynamic(() => import("../components/homepage/section1"));
 const SecondSection = dynamic(() => import("../components/homepage/section2"));
 
 export default function Home() {
-  const [isClient, setIsClient] = useState(false);
+  // const [isClient, setIsClient] = useState(false);
   const section2Ref = useRef<HTMLParagraphElement | null>(null);
   const Section2RefValue = useOnScreen(section2Ref);
   const [isSection2Visible, setIsSection2Visible] = useState(false);
@@ -20,11 +20,11 @@ export default function Home() {
     }
   }, [Section2RefValue, isSection2Visible]);
 
-  useEffect(() => {
-    // setTimeout(() =>
-    setIsClient(true);
-    // , 1000)
-  }, []);
+  // useEffect(() => {
+  //   // setTimeout(() =>
+  //   setIsClient(true);
+  //   // , 1000)
+  // }, []);
 
   return (
     <>
@@ -35,7 +35,7 @@ export default function Home() {
       {/* {!isClient ? null : ( */}
       <main className="z-0 h-full">
         <div className="h-screen snap-y snap-mandatory overflow-scroll">
-          <FirstSection />
+          <Section1 />
           <SecondSectionContainer ref={section2Ref}>
             {isSection2Visible && <SecondSection />}
           </SecondSectionContainer>

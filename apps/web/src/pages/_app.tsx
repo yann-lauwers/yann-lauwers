c2aea2283ae92f34d1f446bbec8fcf5eb8e66803
Lauwers Yann <76515349+yann-embie@users.noreply.github.com>
2023-05-03 08:54:04 +0200

    fix: styles from ui package

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import "ui/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
